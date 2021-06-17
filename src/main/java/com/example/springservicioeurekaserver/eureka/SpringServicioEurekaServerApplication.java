e6058ff341f5b94d9b79f1422713114e0d6bf162
jscoder <javier.ant.sanchezc@gmail.com>
2021-06-17 05:04:21 -0500

    Initial commit

---
+++ src/main/java/com/example/springservicioeurekaserver/eureka/SpringServicioEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springservicioeurekaserver.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServicioEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServicioEurekaServerApplication.class, args);
+    }
+
+}
