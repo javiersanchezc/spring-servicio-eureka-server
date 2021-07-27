0232321d85d85a29c5dd47131094dda81c92915e
jscoder <javier.ant.sanchezc@gmail.com>
2021-07-26 20:38:13 -0500

    :bug: la soluciuon de un bug

--- src/main/java/com/example/springservicioeurekaserver/eureka/SpringServicioEurekaServerApplication.java
+++ src/main/java/com/example/springservicioeurekaserver/eureka/SpringServicioEurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringServicioEurekaServerApplication {
-
+// esto es una prueba
     public static void main(String[] args) {
         SpringApplication.run(SpringServicioEurekaServerApplication.class, args);
     }
